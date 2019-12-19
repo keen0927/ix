0412b833708f7de2b3ee6b627217797c64d0fedc
KEEN <kyh0927@live.co.kr>
2019-12-19 23:45:59 +0900

    191219

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Test from '../components/Test';
+import styled from 'styled-components';
+
+interface IStyle {
+    isActive: boolean
+}
+
+const AppBlock = styled.div<IStyle>`
+    width: 100px;
+    height: 200px;
+    border: 1px solid #000;
+    background-color: ${props => (props.isActive ? 'red' : '')};
+`;
 
 const App = () => {
+
+    const [isActive, setIsActive] = useState(false);
+
+    const onClick = () => setIsActive(!isActive);
+
     return (
-        <div>
+        <AppBlock isActive={isActive} onClick={onClick}>
             <Test />
-        </div>
+        </AppBlock>
     );
 };
 
