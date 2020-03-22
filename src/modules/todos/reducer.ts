28e18502fd906581e87a36902fdbea0ab080bee4
KEEN <kyh0927@live.co.kr>
2020-03-22 22:27:19 +0900

    Feat: Reducer 추가

--- src/modules/todos/reducer.ts
+++ src/modules/todos/reducer.ts
@@ -5,16 +5,31 @@ import {
 	LOAD_LIST_REQUEST,
 	LOAD_LIST_SUCCESS,
 	LOAD_LIST_FAILURE,
+	ADD_LIST_REQUEST,
+	ADD_LIST_SUCCESS,
+	ADD_LIST_FAILURE,
+	TOGGLE_LIST_REQUEST,
+	TOGGLE_LIST_SUCCESS,
+	TOGGLE_LIST_FAILURE,
+	REMOVE_LIST_REQUEST,
+	REMOVE_LIST_SUCCESS,
+	REMOVE_LIST_FAILURE
 } from './actions';
 
 interface InitialStateProps {
 	lists: Todo[];
 	page: number;
+	isAddingList: boolean;
+	isTogglingList: boolean;
+	isRemovingList: boolean;
 }
 
 const initialState: InitialStateProps = {
 	lists: [],
 	page: 1,
+	isAddingList: false,
+	isTogglingList: false,
+	isRemovingList: false
 };
 
 const todos = (state = initialState, action: LoadListAction) => {
@@ -31,9 +46,39 @@ const todos = (state = initialState, action: LoadListAction) => {
 			case LOAD_LIST_FAILURE: {
 				break;
 			}
+			case ADD_LIST_REQUEST: {
+				break;
+			}
+			case ADD_LIST_SUCCESS: {
+				break;
+			}
+			case ADD_LIST_FAILURE: {
+				break;
+			}	
+			case TOGGLE_LIST_REQUEST: {
+				break;
+			}
+			case TOGGLE_LIST_SUCCESS: {
+				break;
+			}
+			case TOGGLE_LIST_FAILURE: {
+				break;
+			}	
+			case REMOVE_LIST_REQUEST: {
+				break;
+			}
+			case REMOVE_LIST_SUCCESS: {
+				break;
+			}
+			case REMOVE_LIST_FAILURE: {
+				break;
+			}									
 			default: {
 				break;
 			}
+
+
+			
 		}
 	});
 };
